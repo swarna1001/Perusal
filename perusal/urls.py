6f5fde84a84858844d1eca8687254da2c87cd4fd
swarna1001 <66565400+swarna1001@users.noreply.github.com>
2020-09-20 16:55:11 +0530

    1.7 - added login and signup links to the index template (latest working version) (trial)

--- perusal/urls.py
+++ perusal/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import url, include
-#from accounts import views as v
+from accounts import views as v
 
 from .import views
 
