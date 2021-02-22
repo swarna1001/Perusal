f0fe142e84a68ffd738313aa34b9530c680015bf
swarna1001 <66565400+swarna1001@users.noreply.github.com>
2021-02-22 18:33:19 +0530

    2.8 - design changes in the profile page

--- accounts/views.py
+++ accounts/views.py
@@ -71,11 +71,51 @@ def logout_view(request):
 		return render(request, 'accounts/logout.html')
 
 
-
 @login_required
 def my_profile_view(request):
+
+	users = Profile.objects.exclude(user = request.user)
+	sent_friend_requests = FriendRequest.objects.filter(from_user = request.user)
+	sent_to = []
+	friends = []
+	for user in users:
+		friend = user.friends.all()	
+		for f in friend:
+			if f in friends:
+				friend = friend.exclude(user = f.user)
+
+		friends += friend
+
+	my_friends = request.user.profile.friends.all()
+	for i in my_friends:
+		if i in friends:
+			friends.remove(i)
+
+	if request.user.profile in friends:
+		friends.remove(request.user.profile)
+
+	random_list = random.sample(list(users), min(len(list(users)), 10))
+
+	for r in random_list:
+		if r in friends:
+			random_list.remove(r)
+
+	friends += random_list
+
+	for i in my_friends:
+		if i in friends:
+			friends.remove(i)
+
+	for sent in sent_friend_requests:
+		sent_to.append(sent.to_user)
+
+	context = {
+			'users' : friends,
+			'sent' : sent_to
+	}
+
 	
-	return render(request, 'accounts/my_profile.html')
+	return render(request, 'accounts/my_profile.html', context)
 
 
 @login_required
